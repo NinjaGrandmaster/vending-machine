1f9e0f95ae3adff4f90adaf31193e812afe43d99
NinjaGrandmaster <arenas1944@gmail.com>
2021-12-27 14:46:45 -0800

    Added command line argument parameters

--- VendingMachineC/main.c
+++ VendingMachineC/main.c
@@ -1,19 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 #include "list.h"
 
 double getCoins(double price);
 
-int main() {
-
-    int numItems = 3; /* counter to keep track of number of items in vending machine and for assigning keys to additonal items*/
-    int vendingAction; /* used to ask user the vending action the would like to perform - purchase, add, exit. */
-    int itemNum; /* used to ask user which item the would like form vending machine */
-    double newItemPrice;
-    char newItemName[25];
-
+int main(int argc, char *argv[]) {
 
     struct Node* vendItems = NULL; /* list containing all items in vending machine*/
 
@@ -22,70 +16,93 @@ int main() {
     append(&vendItems, 2, "Chocolate", 1.25);
     append(&vendItems, 3, "Bagel", 2.50);
 
-    while (1) {
-        /* ask user if they are purchasing or adding item to machine*/
-        printf("Select an action \n1: Puchase Item\n2: Add Item\n3: Exit\n");
-        scanf("%d", &vendingAction);
+    if( argc == 2 ) {
+        printf("The argument supplied is %s\n", argv[1]);
 
-        switch (vendingAction)
-        {
-            case 1:
-                /* display items in vending machine*/
-                printList(vendItems);
-                /* get item selected by user */
-                printf("Select an Item\n");
-                scanf("%d", &itemNum);
-
-                bool itemExists = exists(vendItems, itemNum); /* check if items exist */
-
-                if (itemExists)
-                {
-                    /* get name and price of item from list */
-                    char* itemName = getName(vendItems, itemNum);
-                    double itemPrice = getPrice(vendItems, itemNum);
-
-                    printf("Selected: %s - $%.2f\n", itemName, itemPrice);
-
-                    double enteredCurrency = getCoins(itemPrice); /* get coins from user */
-
-                    printf("\nVending %s\n\n", itemName); /* vend item */
-
-                    /* calculate and return change*/
-                    double change = enteredCurrency - itemPrice;
-                    printf("Dispencing change\n");
-                    printf("Change = $%.2f\n\n", change);
-
-                } else {
-                    /* item does not exist */
-                    printf("Invalid Choice\n");
-                }
-
-                break;
-
-            case 2:
-                printf("Enter item name: ");
-                scanf("%s", newItemName);
-
-                printf("Enter item price: ");
-                scanf("%lf", &newItemPrice);
-
-                numItems += 1; /* increment num items counter */
-
-                append(&vendItems, numItems, newItemName, newItemPrice); /* add item to list */
-
-                printf("Item added: %s - $%.2f\n\n", newItemName, newItemPrice);
-                break;
-            
-            case 3:
-                freeList(vendItems); /* free memory used to store list on exit*/
-                exit(0);
-                break;
-            
-            default:
-                printf("invalid choice\n");
-                break;
+        if (!strcmp("-l", argv[1])){
+            printList(vendItems);
+        } else {
+            printf("Unknown commands\n");
         }
+        
     }
+    else if( argc > 3 ) {
+        printf("Too many arguments supplied.\n");
+    }
+    else {
+        printf("Running interactive mode.\n");
+
+        int numItems = 3; /* counter to keep track of number of items in vending machine and for assigning keys to additonal items*/
+        int vendingAction; /* used to ask user the vending action the would like to perform - purchase, add, exit. */
+        int itemNum; /* used to ask user which item the would like form vending machine */
+        double newItemPrice; /* new peice entered by user */
+        char newItemName[25]; /* name of new item entered by user */
+
+        while (1) {
+            /* ask user if they are purchasing or adding item to machine*/
+            printf("Select an action \n1: Puchase Item\n2: Add Item\n3: Exit\n");
+            scanf("%d", &vendingAction);
+
+            switch (vendingAction)
+            {
+                case 1:
+                    /* display items in vending machine*/
+                    printList(vendItems);
+                    /* get item selected by user */
+                    printf("Select an Item\n");
+                    scanf("%d", &itemNum);
+
+                    bool itemExists = exists(vendItems, itemNum); /* check if items exist */
+
+                    if (itemExists)
+                    {
+                        /* get name and price of item from list */
+                        char* itemName = getName(vendItems, itemNum);
+                        double itemPrice = getPrice(vendItems, itemNum);
+
+                        printf("Selected: %s - $%.2f\n", itemName, itemPrice);
+
+                        double enteredCurrency = getCoins(itemPrice); /* get coins from user */
+
+                        printf("\nVending %s\n\n", itemName); /* vend item */
+
+                        /* calculate and return change*/
+                        double change = enteredCurrency - itemPrice;
+                        printf("Dispencing change\n");
+                        printf("Change = $%.2f\n\n", change);
+
+                    } else {
+                        /* item does not exist */
+                        printf("Invalid Choice\n");
+                    }
+
+                    break;
+
+                case 2:
+                    printf("Enter item name: ");
+                    scanf("%s", newItemName);
+
+                    printf("Enter item price: ");
+                    scanf("%lf", &newItemPrice);
+
+                    numItems += 1; /* increment num items counter */
+
+                    append(&vendItems, numItems, newItemName, newItemPrice); /* add item to list */
+
+                    printf("Item added: %s - $%.2f\n\n", newItemName, newItemPrice);
+                    break;
+                
+                case 3:
+                    freeList(vendItems); /* free memory used to store list on exit*/
+                    exit(0);
+                    break;
+                
+                default:
+                    printf("invalid choice\n");
+                    break;
+            }
+        }
+    } /* end of inteactive mode */
 
     return 0;
 } /* end of main*/
