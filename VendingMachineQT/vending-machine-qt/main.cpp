dd139bbb90a1b9b619d1a11573ec67eeb059b798
NinjaGrandmaster <arenas1944@gmail.com>
2021-12-29 15:39:26 -0800

    Added example of reading standard output from external program

--- VendingMachineQT/vending-machine-qt/main.cpp
+++ VendingMachineQT/vending-machine-qt/main.cpp
@@ -1,10 +1,30 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QProcess>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    // set path to external program here
+    QString program = "";
+
+    QStringList arguments;
+    arguments<<"-p" << "val";
+
+    QProcess *myProcess = new QProcess();
+
+    QProcess::connect(myProcess, QOverload<int, QProcess::ExitStatus>::of(&QProcess::finished),
+                      [=](int exitCode, QProcess::ExitStatus exitStatus){
+        qDebug()<<exitCode;
+        qDebug()<<exitStatus;
+        qDebug()<<myProcess->readLine();
+    });
+
+    myProcess->start(program, arguments);
+
     MainWindow w;
     w.show();
     return a.exec();
